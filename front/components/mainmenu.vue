de457c896ad1eba5479079e5ac91c7ede2cc3b02
Tehnikumi Opilane <opilane@tptlive.ee>
2019-03-21 10:55:26 +0200

    Recent changes

---
+++ front/components/mainmenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a v-for="link in links" class="navbar-item" :href="link.link" :class="{'is-active': routeActive(link.link)}">{{link.title}}</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "mainmenu",
+    data() {
+      return {
+        links: [
+          {title: 'Index', link: '/'},
+          {title: 'Posts', link: '/posts'}
+        ]
+      }
+    },
+    methods: {
+      routeActive(link){
+        return link == this.$route.fullPath;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .menu {
+    width: 20%;
+  }
+</style>
